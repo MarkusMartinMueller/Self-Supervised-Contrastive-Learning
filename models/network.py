b3dd4dde2890ffd43ef0699ac432f97826c56352
markus_2311_mueller <markus.m.mueller@campus.tu-berlin.de>
2021-11-12 09:01:03 +0100

    conv1 one layer for joint ResNet50

--- models/network.py
+++ models/network.py
@@ -4,21 +4,19 @@
   Created: 202-11-09
 """
 
-
-
 import torch
 import torch.nn as nn
 import torchvision.models as models
 from ResNet import ResNet50_S1, ResNet50_S2, ResNet50_joint
 
 class TwoBranch(nn.Module):
-    def __init__(self,encoder_s1,encoder_s2,encoder_joint,projection_dim,n_features):
-        super(TwoBranch,self).__init__()
+
+    def __init__(self, encoder_s1, encoder_s2, encoder_joint, projection_dim, n_features):
+        super(TwoBranch, self).__init__()
         """
         As encoder the commonly used ResNet50 is adopted to obtain hi = f(xi)
         As projection head a mlp with one hidden layer is used to obtain g(hi) = W_2(sigma(W_1hi) with sigma = ReLU
         """
-
 
         self.encoder_s1 = encoder_s1
         self.encoder_s2 = encoder_s2
@@ -27,9 +25,9 @@ class TwoBranch(nn.Module):
 
         # add mlp projection head
 
-        self.projection_head_s1 = nn.Sequential(nn.Linear(self.n_features,self.n_features,bias = False),
+        self.projection_head_s1 = nn.Sequential(nn.Linear(self.n_features, self.n_features, bias=False),
                                              nn.ReLU(),
-                                             nn.Linear(self.n_features,projection_dim)
+                                             nn.Linear(self.n_features, projection_dim)
                                                 )
 
         self.projection_head_s2 = nn.Sequential(nn.Linear(self.n_features, self.n_features, bias=False),
@@ -55,7 +53,7 @@ class TwoBranch(nn.Module):
             return self.forward_joint(s_1, s_2)
 
 
-    def forward_separate(self,s_1,s2):
+    def forward_separate(self,s_1,s_2):
 
         h_i = self.encoder_s1(s_1)
         h_j = self.encoder_s2(s_2)
@@ -64,7 +62,7 @@ class TwoBranch(nn.Module):
         h_i = torch.flatten(h_i, 1)
         h_j = torch.flatten(h_j, 1)
 
-        ##output dim could be e.g. [128]
+        ##output dim could be e.g. [batch_size,128]
         projection_i = self.projection_head_s1(h_i)
         projection_j = self.projection_head_s2(h_j)
 
@@ -79,7 +77,7 @@ class TwoBranch(nn.Module):
         h_i = torch.flatten(h_i, 1)
         h_j = torch.flatten(h_j, 1)
 
-        ##output dim could be e.g. [128]
+        ##output dim could be e.g. [batch_size,128]
         projection_i = self.projection_head_s1(h_i)
         projection_j = self.projection_head_s2(h_j)
 
@@ -98,9 +96,9 @@ if __name__ == "__main__":
     resnet_s1 = ResNet50_S1()
     resnet_s2 = ResNet50_S2()
     resnet_joint = ResNet50_joint()
-    net= TwoBranch( resnet_s2,resnet_s1,resnet_joint,n_features = 2048, projection_dim= 128)
+    net = TwoBranch(resnet_s2, resnet_s1, resnet_joint, n_features=2048, projection_dim=128)
 
-    h_i, h_j, projection_i, projection_j = net(inputs_s2,inputs_s1)
+    h_i, h_j, projection_i, projection_j = net(inputs_s2, inputs_s1)
     print(h_i.shape, h_j.shape, projection_i.shape, projection_j.shape)
 
 
