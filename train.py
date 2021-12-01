c529811cd05f91095e2b1a06041bcaec65ddf075
markus_2311_mueller <markus.m.mueller@campus.tu-berlin.de>
2021-12-01 11:54:04 +0100

    works on erde server now as well

--- train.py
+++ train.py
@@ -8,45 +8,47 @@ from datetime import datetime
 from tensorboardX import SummaryWriter
 import os
 
-#local imports
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+# local imports
 
 
 ##local imports
 
-from models.network import get_model
-from data.data_one_way import dataGenBigEarthLMDB_joint
-from loss.get_loss import get_loss_func
 
-from utils.fusion import get_fusion
-from utils.utils import parse_config
-from utils.utils import MetricTracker
-from utils.utils import save_params
-from utils.scheduler import get_scheduler
-from utils.optimizer import get_optimizer
-from utils.utils import  save_checkpoint
+from utils import get_fusion
+from utils import parse_config
+from utils import MetricTracker
+from utils import save_params
+from utils import get_scheduler
+from utils import get_optimizer
+from utils import save_checkpoint
+
+from models import get_model
+
+from data import dataGenBigEarthLMDB_joint
+from loss import get_loss_func
+
 
 def main(filename):
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     print("Using {} device".format(device))
 
     config = parse_config(filename)
 
     ## tensorboard preparations
     save_path = os.path.join(config['logging_params']['save_dir'], config['name'],
-                           config['logging_params']['name'])
+                             config['logging_params']['name'])
     print('saving file name is ', save_path)
 
-    checkpoint_dir = os.path.join( save_path, 'checkpoints')
+    checkpoint_dir = os.path.join(save_path, 'checkpoints')
     train_writer = SummaryWriter(os.path.join(save_path, 'training'))
     val_writer = SummaryWriter(os.path.join(save_path, 'val'))
     ## tensorboard preparations
 
-
     ### data generation data loader preperation
     train_dataGen = dataGenBigEarthLMDB_joint(
-        bigEarthPthLMDB_S2="C:/Users/Markus/Desktop/project/data/BigEarth_Serbia_Summer_S2.lmdb",
-        bigEarthPthLMDB_S1="C:/Users/Markus/Desktop/project/data/BigEarth_Serbia_Summer_S1.lmdb",
+        bigEarthPthLMDB_S2="/media/storagecube/markus/project/data/BigEarth_Serbia_Summer_S2.lmdb",
+        bigEarthPthLMDB_S1="/media/storagecube/markus/project/data/BigEarth_Serbia_Summer_S1.lmdb",
         state='train',
         train_csv=config["train_csv"],
         val_csv=config["val_csv"],
@@ -54,31 +56,28 @@ def main(filename):
     )
 
     val_dataGen = dataGenBigEarthLMDB_joint(
-        bigEarthPthLMDB_S2="C:/Users/Markus/Desktop/project/data/BigEarth_Serbia_Summer_S2.lmdb",
-        bigEarthPthLMDB_S1="C:/Users/Markus/Desktop/project/data/BigEarth_Serbia_Summer_S1.lmdb",
+        bigEarthPthLMDB_S2="/media/storagecube/markus/project/data/BigEarth_Serbia_Summer_S2.lmdb",
+        bigEarthPthLMDB_S1="/media/storagecube/markus/project/data/BigEarth_Serbia_Summer_S1.lmdb",
         state='val',
         train_csv=config["train_csv"],
         val_csv=config["val_csv"],
         test_csv=config["test_csv"]
     )
 
-
     train_data_loader = DataLoader(train_dataGen, config["batch_size"], num_workers=0, shuffle=True, pin_memory=True)
     val_data_loader = DataLoader(val_dataGen, config["batch_size"], num_workers=0, shuffle=True, pin_memory=True)
 
     ### data generation data loader preperation
 
-
     ## model,optimizer, scheduler, loss_func initilization
 
-    model = get_model(config["type"],config["n_features"],config["projection_dim"],config["out_channels"])
+    model = get_model(config["type"], config["n_features"], config["projection_dim"], config["out_channels"])
     model.to(device)
 
-    optimizer = get_optimizer(model, config["optimizer"],config["learning_rate"],config["weight_decay"])
-    #scheduler = get_scheduler(optimizer, config["schedluer_gamma"]
-    loss_func = get_loss_func(config["loss_func"],config["projection_dim"],config["fusion"])
-
-
+    optimizer = get_optimizer(model, config["optimizer"], config["learning_rate"], config["weight_decay"])
+    # scheduler = get_scheduler(optimizer, config["schedluer_gamma"]
+    loss_func = get_loss_func(config["loss_func"], config["projection_dim"], config["fusion"])
+    loss_func.to(device)
 
     ## save params in yaml file
     save_params(config)
@@ -86,29 +85,27 @@ def main(filename):
     min_val_loss = math.inf
 
     for epoch in range(config["start_epoch"], config["epochs"]):
-        print('Epoch {}/{}'.format(epoch+1, config["epochs"]))
+        print('Epoch {}/{}'.format(epoch + 1, config["epochs"]))
         print('-' * 10)
 
-        train(model, train_data_loader, loss_func, optimizer, epoch, train_writer, config)
+        train(model, train_data_loader, loss_func, optimizer, epoch, train_writer, config, device)
 
         if epoch % 2 == 0:
 
-            val_loss = val(val_data_loader, model, loss_func, epoch, val_writer, config)
+            val_loss = val(val_data_loader, model, loss_func, epoch, val_writer, config, device)
 
-            if val_loss < min_val_loss :
-
+            if val_loss < min_val_loss:
                 save_checkpoint({
                     'epoch': epoch,
                     'state_dict': model.state_dict(),
                     'optimizer': optimizer.state_dict(),
-                }, checkpoint_dir )
+                }, checkpoint_dir)
 
                 min_val_loss = val_loss
 
-def train(model, trainloader,loss_func , optimizer,  epoch,train_writer,config):
 
+def train(model, trainloader, loss_func, optimizer, epoch, train_writer, config, device):
     loss_tracker = MetricTracker()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model.train()
 
@@ -118,22 +115,18 @@ def train(model, trainloader,loss_func , optimizer,  epoch,train_writer,config):
 
         labels = batch['labels'].to(device)
 
-        h_i, h_j, projection_i, projection_j = model(imgs_S1,imgs_S2) #projection_i and _j are the outputs after the mlp heads
+        h_i, h_j, projection_i, projection_j = model(imgs_S1,
+                                                     imgs_S2)  # projection_i and _j are the outputs after the mlp heads
 
-
-        fused = get_fusion(config["fusion"],projection_i,projection_j)
-        loss = loss_func(fused,labels)
-
+        fused = get_fusion(config["fusion"], projection_i, projection_j)
+        loss = loss_func(fused, labels)
 
         ### detach gradients
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-
-
-        loss_tracker.update(loss.item() )
-
+        loss_tracker.update(loss.item())
 
     info = {
         "Loss": loss_tracker.avg,
@@ -146,14 +139,13 @@ def train(model, trainloader,loss_func , optimizer,  epoch,train_writer,config):
         loss_tracker.avg
     ))
 
-    #if config['scheduler_gamma']:
-        #scheduler.step()
+    # if config['scheduler_gamma']:
+    # scheduler.step()
 
 
-def val(valloader, model, loss_func , epoch, val_writer,config):
-
+def val(valloader, model, loss_func, epoch, val_writer, config, device):
     model.eval()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     loss_tracker = MetricTracker()
 
     with torch.no_grad():
@@ -163,7 +155,7 @@ def val(valloader, model, loss_func , epoch, val_writer,config):
 
             labels = batch['labels'].to(device)
 
-            h_i, h_j, projection_i, projection_j = model(imgs_S1,imgs_S2)
+            h_i, h_j, projection_i, projection_j = model(imgs_S1, imgs_S2)
             # projection_i and j are the outputs after the mlp heads
 
             fused = get_fusion(config["fusion"], projection_i, projection_j)
@@ -183,16 +175,5 @@ def val(valloader, model, loss_func , epoch, val_writer,config):
     return loss
 
 
-
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
-
-    main("/config/args.yaml")
+    main("/media/storagecube/markus/project/config/args.yaml")
