124aca4bf6b8afaa1c0ba5b996bfe2aba86fea20
markus_2311_mueller <markus.m.mueller@campus.tu-berlin.de>
2021-12-03 12:21:01 +0100

    saves image tensors not projection heads

--- Retrieval/build_archive.py
+++ Retrieval/build_archive.py
@@ -8,12 +8,14 @@ import pickle
 # local imports
 from utils import get_fusion
 from utils import parse_config
-from models import TwoBranch, get_model
+from models import  get_model
 from data import dataGenBigEarthLMDB_joint
 
 
 def build_db(filename, state_dict_path,state):
     """
+    To build the archive at the moment the value of batch_size has to be set to 1, so that
+    each image is saved at the time
 
     :param filename: contains path to parameters.yaml used to train the model
     :param state:  contains string of either val or test to create val or test archive
@@ -24,6 +26,7 @@ def build_db(filename, state_dict_path,state):
 
     # initialize the model
     config = parse_config(filename)
+
     model = get_model(config["type"], config["n_features"], config["projection_dim"], config["out_channels"])
     model.to(device)
 
@@ -36,7 +39,7 @@ def build_db(filename, state_dict_path,state):
         model.load_state_dict(torch.load(state_dict_path, map_location=torch.device('cpu'))["state_dict"])
 
     retrieve_dataGen = dataGenBigEarthLMDB_joint(
-        bigEarthPthLMDB_S2="C:/Users/Markus/Desktop/project/data/BigEarth_Serbia_Summer_S2.lmdb",
+        bigEarthPthLMDB_S2 = "C:/Users/Markus/Desktop/project/data/BigEarth_Serbia_Summer_S2.lmdb",
         bigEarthPthLMDB_S1="C:/Users/Markus/Desktop/project/data/BigEarth_Serbia_Summer_S1.lmdb",
         state="val",
         train_csv=config["train_csv"],
@@ -65,12 +68,15 @@ def build_db(filename, state_dict_path,state):
 
 
 
-            feature_dict[idx] = (projection_i, projection_j,fused,labels)
-    pickle.dump(feature_dict, open("archive_db.p", "wb"))
+            #feature_dict[idx] = (projection_i, projection_j,fused,labels)
+            feature_dict[idx] = (imgs_S1, imgs_S2, fused, labels)
+    pickle.dump(feature_dict, open("archive_separate_avg.p", "wb"))
 
 
 if __name__ == '__main__':
-    build_db(filename="/logs/Resnet50/first_test/parameters.yaml", state_dict_path="/logs/Resnet50/first_test/checkpoints_model_best.pth.tar", state="val")
-    #feature_dict = pickle.load(open("archive_db.p", "rb"))
+    build_db(filename="C:/Users/Markus/Desktop/project/logs/Resnet50/separate_avg/parameters.yaml", state_dict_path="C:/Users/Markus/Desktop/project/logs/Resnet50/separate_avg/checkpoints_model_best.pth.tar", state="val")
+    feature_dict = pickle.load(open("archive_separate_avg.p", "rb"))
+
+    print(feature_dict)
 
 
