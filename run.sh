0139b85d2ec390727c81f81903870a5aeb1c8ed9
markus_2311_mueller <markus.m.mueller@campus.tu-berlin.de>
2022-01-04 09:26:48 +0100

    added right path for ptyhon utils/utils.py

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ python train.py --filepath /media/storagecube/markus/project/args.yaml
 
 python retrieval.py --filepath /media/storagecube/markus/project/logs/test/joint_concat_adam/parameters.yaml
 
-python check_h5_metric --filepath /media/storagecube/markus/project/logs/test/joint_concat_adam/retrieval.h5
+python utils/utils.py --filepath /media/storagecube/markus/project/logs/test/joint_concat_adam/retrieval.h5
